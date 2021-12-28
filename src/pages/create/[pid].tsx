458d79fe3bb9a8f797f8151df2af979951ad7fda
ZzRanger <calebmarklim@gmail.com>
2021-12-28 11:31:19 -0600

    remove unused file

--- src/pages/create/[pid].tsx
+++ src/pages/create/[pid].tsx
@@ -1,13 +1,12 @@
 import Header from "../../components/layouts/header/header";
 import TierlistContainer from "../../components/tierlist/TierlistContainer";
-import UploadTest from "../../components/UploadTest";
 
 export default function TierlistPage() {
     return (
       <div className=" bg-gradient-to-b from-indigo-500 to-black text-white">
         <Header />
         <TierlistContainer />  
-        <UploadTest />
+       
         {/* <Footer /> */}
       </div>
     );
